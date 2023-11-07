dd21225f5cb2e11e0001dc8580c4694011403779
John <johnolshea@gmail.com>
2023-11-06 18:56:03 -0800

    fixed macro

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,28 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef DOG
+#define DOG
 
 /**
- * struct dog - properties of a dog
- * @name: pointer to dog name
- * @age: dog's age
- * @owner: pointer to the owner of the dog
+ * struct dog - dog struct
+ *
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 
 #endif
